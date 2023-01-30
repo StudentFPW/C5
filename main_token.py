3ce666bf7a62df14d545cbd7a45d1d352756dc06
LeoN.git.user <jofeleonids00@gmail.com>
2023-01-30 23:12:28 +0200

    30/01/2023 23:12

---
+++ main_token.py
@@ -0,0 +1,10 @@
+# Importing the `telebot` library.
+import telebot
+
+# A bot that helps you create new bots and change their settings: https://t.me/BotFather
+
+# Use this token to access the HTTP API: ↓
+TOKEN = ""
+
+# Creating a bot object.
+bot = telebot.TeleBot(TOKEN)
