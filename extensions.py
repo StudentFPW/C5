bb5de363c821cbb379d0d660ade193c67dd99448
LeoN.git.user <jofeleonids00@gmail.com>
2023-02-02 20:14:14 +0200

    02/02/2023 20:14

--- extensions.py
+++ extensions.py
@@ -1,8 +1,6 @@
 # Бота разработал студент (Leonids Jofe) из школы SkillFactory, курс Full-stack python developer, класс FPW-104
 
 # Он импортирует переменную бота из файла config.py.
-import telebot.types
-
 from config import bot
 
 # Он импортирует класс Currency из файла currency.py.
@@ -26,7 +24,7 @@ class Currencies:
 # ----------------------------------------------------------------------------------------------------------------------
 
 @bot.message_handler(commands=["start", "help"])
-def helper(message: telebot.types.Message) -> None:
+def helper(message) -> None:
     command_list = "<имя валюты, цену которой вы хотите узнать, EUR>\n" \
                    "<имя валюты, в которой надо узнать цену первой валюты, USD>\n" \
                    "<количество первой валюты, 5>\n" \
@@ -41,7 +39,7 @@ def helper(message: telebot.types.Message) -> None:
 # ----------------------------------------------------------------------------------------------------------------------
 
 @bot.message_handler(commands=["value"])
-def list_of_currencies(message: telebot.types.Message) -> None:
+def list_of_currencies(message) -> None:
     """
     Он отправляет пользователю сообщение со списком всех доступных валют.
 
@@ -57,7 +55,7 @@ def list_of_currencies(message: telebot.types.Message) -> None:
 # ----------------------------------------------------------------------------------------------------------------------
 
 @bot.message_handler(content_types=["text"])
-def starter(message: telebot.types.Message) -> None:
+def starter(message) -> None:
     try:
         text = message.text.upper().split(" ")
         if len(text) == 3:
